0ca288f0db2bf546b9974014fc8622453fb06662
Ahmad Rahimie <newrehmi@gmail.com>
2015-10-18 12:51:42 +0800

    Session instantiation storage check fix

--- Exedra/Application/Session/Session.php
+++ Exedra/Application/Session/Session.php
@@ -19,13 +19,15 @@ class Session
 	public function __construct(&$storage = null)
 	{
 		## set storage. default use php _SESSION, if not passed through constructor param.
-		if($storage)
+		if($storage !== null)
 		{
 			$this->storage = &$storage;
 		}
 		else
 		{
-			session_start();
+			if(!isset($_SESSION))
+				session_start();
+
 			$this->storage = &$_SESSION;
 		}
 	}
