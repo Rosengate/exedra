3f47766f1d1610b472984d46a2775be3bf0239d6
Ahmad Rahimie <newrehmi@gmail.com>
2015-10-18 12:52:06 +0800

    add test for Session manager

---
+++ tests/SessionTest.php
@@ -0,0 +1,80 @@
+<?php
+require_once "Exedra/Exedra.php";
+
+class SessionTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp()
+	{
+		$this->storage = array();
+
+		$this->session = new \Exedra\Application\Session\Session($this->storage);
+	}
+
+	public function testAssign()
+	{
+		$this->session->set('test_key', 'test_value');
+
+		$this->assertEquals($this->storage['test_key'], 'test_value');
+
+		$this->assertEquals($this->storage['test_key'], $this->session->get('test_key'));
+	}
+
+	public function testNestedAssign()
+	{
+		$this->session->set('nested_test_key.nested_test_key2', 'test_value');
+
+		$this->session->set('nested_test_key.nested_test_key3', array(
+			'nested_test_key4' => 'test_value2'
+			));
+
+		$this->assertEquals(isset($this->storage['nested_test_key']), true);
+
+		$this->assertEquals($this->storage['nested_test_key']['nested_test_key2'], 'test_value');
+
+		$this->assertEquals($this->storage['nested_test_key']['nested_test_key2'], $this->session->get('nested_test_key.nested_test_key2'));
+
+		$this->assertEquals($this->storage['nested_test_key']['nested_test_key3']['nested_test_key4'], 'test_value2');
+	}
+
+	public function testPrefix()
+	{
+		$this->session->setPrefix('test_prefix');
+
+		$this->session->set('test_key', 'test_value');
+
+		$this->assertEquals($this->storage['test_prefix']['test_key'], $this->session->get('test_key'));
+	}
+
+	public function testNestedPrefix()
+	{
+		$this->session->setPrefix('test_prefix.test_prefix2');
+
+		$this->session->get('test_key', 'test_value');
+
+		$this->assertEquals($this->storage['test_prefix']['test_prefix2'], $this->session->get('test_key'));
+	}
+
+	public function testDestroy()
+	{
+		$this->testAssign();
+
+		$this->session->destroy('test_key');
+
+		$this->assertEquals(!isset($this->storage['test_key']), true);
+	}
+
+	public function testDestroyNested()
+	{
+		$this->testNestedAssign();
+
+		$this->session->destroy('nested_test_key.nested_test_key2');
+
+		$this->assertEquals(!isset($this->storage['nested_test_key']['nested_test_key2']), true);
+
+		$this->assertEquals($this->session->has('nested_test_key.nested_test_key3.nested_test_key4'), true);
+
+		$this->session->destroy('nested_test_key.nested_test_key3');
+
+		$this->assertEquals(!isset($this->storage['nested_test_key']['nested_test_key3']['nested_test_key4']), true);
+	}
+}
