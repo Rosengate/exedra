2b444579798fe752de3de7522680a51c0b1f5296
Ahmad Rahimie <newrehmi@gmail.com>
2015-12-26 16:47:35 +0800

    add alias to setMiddleware in Convenient\Route

--- Exedra/Application/Map/Convenient/Route.php
+++ Exedra/Application/Map/Convenient/Route.php
@@ -3,12 +3,28 @@ namespace Exedra\Application\Map\Convenient;
 
 class Route extends \Exedra\Application\Map\Route
 {
-	
-	public function execute($hander)
+	/**
+	 * Alias to setExecute
+	 * @param mixed handler
+	 */
+	public function execute($handler)
 	{
-		return $this->setExecute($hander);
+		return $this->setExecute($handler);
 	}
 
+	/**
+	 * Alias to setMiddleware
+	 * @param mixed middleware handler
+	 */
+	public function middleware($middleware)
+	{
+		return $this->setMiddleware($middleware);
+	}
+
+	/**
+	 * Alias to setSubroutes except that now it require \Closure
+	 * @param \Closure callback
+	 */
 	public function group(\Closure $callback)
 	{
 		return $this->setSubroutes($callback);
