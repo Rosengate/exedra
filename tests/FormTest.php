47088eb13de9c4e32ba9976d9f2777a3896c8250
Ahmad Rahimie <newrehmi@gmail.com>
2016-04-28 01:45:17 +0800

    test: \Exedra\Application\Builder\Form\Form

---
+++ tests/FormTest.php
@@ -0,0 +1,27 @@
+<?php
+require_once "Exedra/Exedra.php";
+
+class FormTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp()
+	{
+		$this->form = new \Exedra\Application\Builder\Form\Form;
+	}
+
+	public function testInputText()
+	{
+		$this->assertEquals('<input type="text" name="foo" id="foo" value="bar"  />', (string) $this->form->text('foo')->value('bar'));
+	}
+
+	public function testInputTextarea()
+	{
+		$this->assertEquals('<textarea name="foo" id="foo" >bar</textarea>', (string) $this->form->textarea('foo')->value('bar'));
+	}
+
+	public function testInputSelect()
+	{
+		$input = $this->form->select('foo')->options(array(1 => 'bar'))->attr('disabled', true);
+
+		$this->assertEquals('<select name="foo" id="foo" disabled="1"><option  value="1">bar</option></select>', (string) $input);
+	}
+}
