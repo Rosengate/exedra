fe6495f672be4a25ad58f956d7b71ab5e66394a7
eimihar <newrehmi@gmail.com>
2016-05-09 02:17:41 +0800

    Blueprint\Path now moved to Factory\Path

--- Exedra/Application/Factory/Path.php
+++ Exedra/Application/Factory/Path.php
@@ -1,25 +1,109 @@
 <?php
 namespace Exedra\Application\Factory;
 
+/**
+ * Simple class for object oriented based path.
+ */
 class Path
 {
 	/**
-	 * @param \Exedra\Loader
+	 * Path of the file.
+	 * @var string|array
 	 */
-	public function __construct(\Exedra\Loader $loader)
+	protected $path;
+
+	public function __construct(\Exedra\Loader $loader, $path = null)
 	{
 		$this->loader = $loader;
+
+		$this->path = $path;
 	}
 
 	/**
-	 * Create Path Instance
-	 * @param string path
-	 * @return Exedra\Application\Blueprint\File
+	 * Check whether this file exists or not.
+	 * @return boolean
+	 */
+	public function isExists()
+	{
+		return $this->loader->has($this->path);
+	}
+
+	/**
+	 * Cast into string
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return $this->toString();
+	}
+
+	/**
+	 * Return a new path with appended one.
+	 * @return \Exedra\Application\Factory\Path
 	 */
 	public function create($path)
 	{
-		return new Blueprint\Path($this->loader, $path);
+		return new static($this->loader, $this->path ? $this->path.'/'.$path : $path);
+	}
+
+	/**
+	 * Get full and usable path for this file.
+	 * @return string
+	 */
+	public function toString()
+	{
+		return $this->loader->buildPath($this->path);
+	}
+
+	/**
+	 * Require this instance's file path extracted with the given data (optional)
+	 * @param array data
+	 * @return mixed
+	 */
+	public function load(array $data = array())
+	{
+		return $this->loader->load($this->path, $data);
+	}
+
+	/**
+	 * Require this file content.
+	 * @return mixed
+	 */
+	public function getContent()
+	{
+		if(!$this->isExists())
+			return false;
+
+		return $this->loader->getContent($this->path);
+	}
+
+	/**
+	 * Alias to getContent()
+	 * @return mixed
+	 */
+	public function getContents()
+	{
+		return $this->getContent();
+	}
+
+	/**
+	 * Put contents to the given path if it's file
+	 * @param string data
+	 * @return mixed
+	 */
+	public function putContents($data = null)
+	{
+		return $this->loader->putContents($this->path, $data);
+	}
+
+	/**
+	 * Create another path when invoked
+	 * @param string
+	 */
+	public function __invoke($path)
+	{
+		return $this->create($path);
 	}
 }
 
-
+?>
--- tests/Factory/PathTest.php
+++ tests/Factory/PathTest.php
@@ -15,11 +15,20 @@ class FactoryPathTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals($this->testPath, __DIR__.DIRECTORY_SEPARATOR.'PathTest.php');
 	}
 
+	public function testMulticreate()
+	{
+		$path = $this->factoryPath->create('folder');
+
+		$newPath = $path->create('doriyaki/dom');
+
+		$this->assertEquals($path.DIRECTORY_SEPARATOR.'doriyaki'.DIRECTORY_SEPARATOR.'dom', (string) $newPath);
+	}
+
 	public function testClassName()
 	{
 		$this->testCreate();
 
-		$this->assertEquals($this->testPath instanceof \Exedra\Application\Factory\Blueprint\Path, true);
+		$this->assertEquals($this->testPath instanceof \Exedra\Application\Factory\Path, true);
 	}
 
 	public function testCheckExists()
@@ -34,5 +43,14 @@ class FactoryPathTest extends PHPUnit_Framework_TestCase
 		$this->testCreate();
 
 		$this->assertEquals($this->testPath->getContent(), file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'PathTest.php'));
+	}
+
+	public function testInvoke()
+	{
+		$path = $this->factoryPath->create('foo');
+
+		$ns = DIRECTORY_SEPARATOR;
+
+		$this->assertEquals($path('bar/baz')->create('qux'), $path.$ns.'bar'.$ns.'baz'.$ns.'qux');
 	}
 }
