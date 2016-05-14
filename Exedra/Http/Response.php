e559749716747fabaf9a1028d357026e1a4d12cf
eimihar <newrehmi@gmail.com>
2016-05-15 00:16:54 +0800

    Http\Response reason phrase set only when needed.

--- Exedra/Http/Response.php
+++ Exedra/Http/Response.php
@@ -5,11 +5,63 @@ class Response extends Message
 {
 	protected $status;
 
+	protected $reasonPhrase = null;
+
 	public function __construct($status = 200, array $headers = array(), Stream $body, $protocol = '1.1', $reason = null)
 	{
 		parent::__construct($headers, $body, $protocol);
 
-		$this->setStatus($status, $reason);
+		$this->statuses =  array(
+			100 => 'Continue',
+			101 => 'Switching Protocols',
+			102 => 'Processing',
+			200 => 'OK',
+			201 => 'Created',
+			202 => 'Accepted',
+			203 => 'Non-Authorative Information',
+			204 => 'No Content',
+			205 => 'Reset Content',
+			206 => 'Partial Content',
+			207 => 'Multi-Status',
+			208 => 'Already Reported',
+			226 => 'IM Used',
+			300 => 'Multiple Choices',
+			301 => 'Moved Permanently',
+			302 => 'Found',
+			303 => 'See Other',
+			304 => 'Not Modified',
+			305 => 'Use Proxy',
+			306 => 'Switch Proxy',
+			307 => 'Temporary Redirect',
+			308 => 'Permanent Redirect',
+			400 => 'Bad Request',
+			401 => 'Unauthorized',
+			402 => 'Payment Required',
+			403 => 'Forbidden',
+			404 => 'Not Found',
+			405 => 'Method Not Allowed',
+			406 => 'Not Acceptable',
+			407 => 'Proxy Authentication Required',
+			408 => 'Request Timeout',
+			409 => 'Conflict',
+			410 => 'Gone',
+			411 => 'Length Required',
+			412 => 'Precondition Failed',
+			413 => 'Request Entity Too Large',
+			414 => 'Request-URI Too Long',
+			415 => 'Unsupported Media Type',
+			416 => 'Request Range Not Satisfied',
+			417 => 'Expectation Failed',
+			500 => 'Internal Server Error',
+			501 => 'Not Implemented',
+			502 => 'Bad Gateway',
+			503 => 'Service Unavailable',
+			504 => 'Gateway Timeout'
+		);
+		
+		$this->status = $status;
+
+		$this->reasonPhrase = $reason;
 	}
 
 	public function __clone()
@@ -27,7 +79,7 @@ class Response extends Message
 		return $this->status;
 	}
 
-	public function setStatus($status, $reason = '')
+	public function setStatus($status, $reason = null)
 	{
 		$this->status = $status;
 
@@ -48,60 +100,12 @@ class Response extends Message
 		if($this->reasonPhrase)
 			return $this->reasonPhrase;
 
-		return $this->getDefaultReasonPhrase($this->status);
+		return isset($this->statuses[$this->status]) ? $this->statuses[$this->status] : 'Unknown';
 	}
 
 	public function getDefaultReasonPhrase($status)
 	{
-		$defaultReasons = array(
-			100 => "Continue",
-			101 => "Switching Protocols",
-			102 => "Processing",
-			200 => "OK",
-			201 => "Created",
-			202 => "Accepted",
-			203 => "Non-Authorative Information",
-			204 => "No Content",
-			205 => "Reset Content",
-			206 => "Partial Content",
-			207 => "Multi-Status",
-			208 => "Already Reported",
-			226 => "IM Used",
-			300 => "Multiple Choices",
-			301 => "Moved Permanently",
-			302 => "Found",
-			303 => "See Other",
-			304 => "Not Modified",
-			305 => "Use Proxy",
-			306 => "Switch Proxy",
-			307 => "Temporary Redirect",
-			308 => "Permanent Redirect",
-			400 => "Bad Request",
-			401 => "Unauthorized",
-			402 => "Payment Required",
-			403 => "Forbidden",
-			404 => "Not Found",
-			405 => "Method Not Allowed",
-			406 => "Not Acceptable",
-			407 => "Proxy Authentication Required",
-			408 => "Request Timeout",
-			409 => "Conflict",
-			410 => "Gone",
-			411 => "Length Required",
-			412 => "Precondition Failed",
-			413 => "Request Entity Too Large",
-			414 => "Request-URI Too Long",
-			415 => "Unsupported Media Type",
-			416 => "Request Range Not Satisfied",
-			417 => "Expectation Failed",
-			500 => "Internal Server Error",
-			501 => "Not Implemented",
-			502 => "Bad Gateway",
-			503 => "Service Unavailable",
-			504 => "Gateway Timeout"
-			);
-
-		return isset($defaultReasons[$status]) ? $defaultReasons[$status] : 'Unknown';
+		return isset($this->statuses[$status]) ? $this->statuses[$status] : 'Unknown';
 	}
 
 	/**
@@ -119,7 +123,10 @@ class Response extends Message
 	 */
 	public function sendHeader()
 	{
-		header('HTTP/'.$this->getProtocolVersion().' '.$this->getStatusCode().' '.$this->getReasonPhrase());
+		if($this->reasonPhrase || $this->protocol != '1.1')
+			header('HTTP/'.$this->getProtocolVersion().' '.$this->status.' '.$this->getReasonPhrase());
+		else
+			http_response_code($this->status);
 
 		foreach($this->headerLines as $key => $values)
 			header($key.': '.implode(', ', $values));
