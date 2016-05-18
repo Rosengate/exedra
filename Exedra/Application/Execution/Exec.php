aa4f5a90c777e1e5207e43eae6b845b1b2c0c0c0
eimihar <newrehmi@gmail.com>
2016-05-18 23:32:49 +0800

    middleware instance builder removal

--- Exedra/Application/Execution/Exec.php
+++ Exedra/Application/Execution/Exec.php
@@ -69,7 +69,6 @@ class Exec extends \Exedra\Container\Container
 		$this->attributes['services']->register(array(
 			'view' => function(){ return new \Exedra\Application\Factory\View($this->getModulePath('View'));},
 			'middlewares' => function() {return $this->app->middleware->getMiddlewares();},
-			'middleware' => function(){ return new \Exedra\Application\Factory\Middleware($this->app->getNamespace(), $this->getModule());},
 			'controller' => function(){ return new \Exedra\Application\Factory\Controller($this->app->getNamespace(), $this->getModule());},
 			'url' => function(){ return new \Exedra\Application\Execution\Factory\Url($this->app->map, $this->request, $this->config->get('app.url', null), $this->config->get('asset.url', null), $this);},
 			"redirect"=> array("\Exedra\Application\Execution\Redirect", array('self')),
