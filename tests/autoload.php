f5ee2871d8e8ddfe6dc956c35304a81563cbcd7b
Ahmad Rahimie <newrehmi@gmail.com>
2016-07-03 18:45:22 +0800

    tests : slash fix

--- tests/autoload.php
+++ tests/autoload.php
@@ -4,8 +4,11 @@ spl_autoload_register(function($class)
 {
 	$path = __DIR__.'/../'.$class.'.php';
 
+	$path = str_replace('\\', '/', $path);
+
 	if(file_exists($path))
 		require_once $path;
+
 });
 
 
