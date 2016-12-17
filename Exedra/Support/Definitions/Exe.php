737c36cbf359c0068d605357388ce302b79214b0
eimihar <newrehmi@gmail.com>
2016-12-17 23:37:15 +0800

    Definitions updated

--- Exedra/Support/Definitions/Exe.php
+++ Exedra/Support/Definitions/Exe.php
@@ -3,10 +3,14 @@ namespace Exedra\Support\Definitions;
 
 use Exedra\Config;
 use Exedra\Container\Registry;
+use Exedra\Factory\Controller;
 use Exedra\Path;
 use Exedra\Runtime\Factory\Form;
 use Exedra\Runtime\Factory\Url;
 use Exedra\Runtime\Redirect;
+use Exedra\Session\Flash;
+use Exedra\Session\Session;
+use Exedra\View\Factory;
 
 /**
  * Properties Definitions Exe container
@@ -19,7 +23,11 @@ use Exedra\Runtime\Redirect;
  * @property Url $url
  * @property Redirect $redirect
  * @property Form $form
+ * @property Factory $view
  * @property Registry[] $services
+ * @property Session $session
+ * @property Flash $flash
+ * @property Controller $controller
  */
 interface Exe
 {
