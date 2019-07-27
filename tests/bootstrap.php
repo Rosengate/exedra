4fad6b24fc50350f6959853f0de5dc0e6a16b9f9
Rahimie Ahmad <newrehmi@gmail.com>
2019-07-27 21:28:34 +0800

    tests set up alias for TestCase

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,3 @@
 <?php
-//if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase'))
-//    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase'))
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
